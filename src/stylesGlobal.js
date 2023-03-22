a3135500f62596fc7b53222d30c22fc664a5ded3
fcventura02 <filipecvr4@gmail.com>
2023-03-22 20:51:55 -0300

    Initi projetct

---
+++ src/stylesGlobal.js
@@ -0,0 +1,24 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body{
+    max-width: 100vw;
+    background-color: #1E192C;
+    color: #fff;
+}
+
+button{
+    border: none;
+    cursor: pointer;
+}
+
+a{
+    cursor: pointer;
+}
+`
